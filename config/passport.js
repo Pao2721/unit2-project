240512343ff55b448fbb14f72bac6ac2ec389deb
Pao2721 <pao2723@gmail.com>
2021-07-26 08:47:47 -0400

    6th

--- config/passport.js
+++ config/passport.js
@@ -4,11 +4,12 @@ import { User } from '../models/user.js'
 import { Profile } from '../models/profile.js'
 
 
+
 passport.use(
  newGoogleStrategy(
   {
-   clientID: process.env.GOOGLE_CLIENT_ID
-   clientSecret: process.env.GOOGLE_SECRET
+   clientID: process.env.GOOGLE_CLIENT_ID,
+   clientSecret: process.env.GOOGLE_SECRET,
    callbackURL: process.env.GOOGLE_CALLBACK
   },
   function (accessToken, refreshToken, profile, done) {
