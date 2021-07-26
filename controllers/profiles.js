8b1ddc323f27221441ac65bd26b84a72319d2185
Pao2721 <pao2723@gmail.com>
2021-07-26 13:13:50 -0400

    fixed git ignore

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -3,7 +3,12 @@ import { Card } from "../models/cards.js"
 import { Collections } from "../models/collections.js"
 
 export{
-
+ index,
+ update,
+ edit,
+ show,
+ removefriend,
+ addFriend
 }
 
 function addFriend(req, res) {
@@ -37,5 +42,60 @@ function removefriend(req, res) {
 }
 
 function update(req, res) {
-  Profile.findByIdandUpdate(req.params.id)
+  Profile.findByIdandUpdate(req.params.id, req.body, {new: true})
+  .then(profile => {
+    res.redirect(`/profiles/${profile._id}`)
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function edit(req, res) {
+  Profile.findById(req.params.id)
+  .then(profile => {
+    res.render('profiles/edit', {
+      title: `Editing ${profile.name}'s profile`,
+      profile: profile
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function show(req, res) {
+  Profile.findById(req.params.id)
+  .populate('friends')
+  .then(profile => {
+    Game.find({ collectedBy: profile._id })
+    .then(games => {
+      Profile.findById(req.user.profile)
+      .then(userProfile => {
+        res.render('profiles/show', {
+          profile,
+          userProfile,
+          title: `${profile.name}'s profile`,
+          games
+        })
+      })
+    })
+
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function index(req, res) {
+  Profile.find({})
+  .then(profiles => {
+    res.render('profiles/index', {
+      title: "Card Collector Profiles",
+      profiles,
+    })
+  })
 }
